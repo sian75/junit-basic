08645879addc25dee62bfc08d9ad6c5c2ff8539e
sian <sian@iezngo.com>
2024-06-09 14:48:19 +0900

    'Book'

--- src/main/java/com/example/demo/domain/Book.java
+++ src/main/java/com/example/demo/domain/Book.java
@@ -1,5 +1,48 @@
 package com.example.demo.domain;
 
-public class Book {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Book {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    @Column(length = 50, nullable = false)
+    private String title;
+    @Column(length = 20, nullable = false)
+    private String author;
+
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
---
+++ src/main/java/com/example/demo/web/dto/BookRespDto.java
@@ -0,0 +1,5 @@
+package com.example.demo.web.dto;
+
+public class BookRespDto {
+
+}
